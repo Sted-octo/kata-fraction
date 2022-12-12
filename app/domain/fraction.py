a2504e009ee2e6ff727672f2a727a02d27db5fd4
Steeve Descarpentries <s.descarpentries@octo.com>
2022-12-12 11:47:31 +0100

    Add test to fraction to be able to create a valid fraction without args

--- app/domain/fraction.py
+++ app/domain/fraction.py
@@ -1,5 +1,5 @@
 class Fraction:
-    def __init__(self, numerator:int, denominator:int=1):
+    def __init__(self, numerator:int=0, denominator:int=1):
       if denominator == 0:
         raise TypeError("denominator can't be equal to 0")
       self.numerator = numerator
--- tests/Fraction_test.py
+++ tests/Fraction_test.py
@@ -7,9 +7,18 @@ def test_Fraction_Without_denominator_should_init_denominator_equal_to_1():
   assert noDenominator.denominator == 1
 
 def test_Fraction_Without_denominator_2_should_init_denominator_equal_to_2():
-  noDenominator:Fraction = Fraction(0,2)
-  assert noDenominator.denominator == 2
+  withDenominator:Fraction = Fraction(0,2)
+  assert withDenominator.denominator == 2
 
 def test_Fraction_Without_denominator_0_should_raise_TypeError_Exception():
   with pytest.raises(TypeError):
     Fraction(0,0)
+
+def test_Fraction_Without_arguments_should_init_numerator_to_0_and_denominator_equal_to_1():
+  noArgs:Fraction = Fraction()
+  assert noArgs.numerator == 0
+  assert noArgs.denominator == 1
+
+def test_Fraction_Without_numerator_2_should_init_numerator_equal_to_2():
+  withDenominator:Fraction = Fraction(2)
+  assert withDenominator.numerator == 2

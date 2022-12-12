f2e31e9beb6cf42fca5b3ac989e24e73e8e056e8
Steeve Descarpentries <s.descarpentries@octo.com>
2022-12-12 15:16:03 +0100

    specify the type of Fraction's variables.

--- app/domain/fraction.py
+++ app/domain/fraction.py
@@ -1,6 +1,6 @@
 class Fraction:
-    def __init__(self, numerator:int=0, denominator:int=1):
-      if denominator == 0:
-        raise TypeError("denominator can't be equal to 0")
-      self.numerator = numerator
-      self.denominator = denominator
+  def __init__(self, numerator:int=0, denominator:int=1):
+    if denominator == 0:
+      raise TypeError("denominator can't be equal to 0")
+    self.numerator:int = numerator
+    self.denominator:int = denominator

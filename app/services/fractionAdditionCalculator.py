b991899bce994819c35c93a2683aad15949c6fef
Steeve Descarpentries <s.descarpentries@octo.com>
2022-12-12 15:34:58 +0100

    Refactor of the code in service fractionAdditionCalculator.py, remove useless service fractionNaturalTester.py and the relative tests.

--- app/services/fractionAdditionCalculator.py
+++ app/services/fractionAdditionCalculator.py
@@ -1,6 +1,5 @@
 from app.domain.fraction import Fraction
 from app.services.fractionNeutralTester import FractionNeutralTester
-from app.services.fractionNaturalTester import FractionNaturalTester
 
 class FractionAdditionCalculator:
   def Add(base:Fraction, toAdd:Fraction) -> Fraction:
@@ -8,9 +7,6 @@ class FractionAdditionCalculator:
       return base
     if FractionNeutralTester.IsNeutral(base):
       return toAdd
-
-    if FractionNaturalTester.IsNatural(base) and FractionNaturalTester.IsNatural(toAdd):
-      return Fraction(base.numerator + toAdd.numerator)
 
     if base.denominator == toAdd.denominator:
       return Fraction(base.numerator + toAdd.numerator, base.denominator)
--- app/services/fractionNaturalTester.py
+++
@@ -1,5 +0,0 @@
-from app.domain.fraction import Fraction
-
-class FractionNaturalTester:
-  def IsNatural(fraction:Fraction) -> bool:
-    return fraction.denominator == 1
